e5648dedf47f4a40b846be4b579df649c8df0ea4
QUentinee <que.heude@gmail.com>
2023-09-20 14:50:20 +0200

    je viens de faire la focntion pour créer un gaulois eh ouais bonhomme

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,6 +1,10 @@
 package personnages;
 
 public class Gaulois {
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
 	private String nom;
 	private int force;
 	private int effetPotion = -1;
