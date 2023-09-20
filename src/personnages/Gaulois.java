c688314ac3af107970d4cc023071e354a28a89b1
QUentinee <que.heude@gmail.com>
2023-09-20 15:24:12 +0200

    commencement du copié-collé qui marche pas encore cest le début des romains ouais ouais

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,11 +1,37 @@
 package personnages;
 
 public class Gaulois {
+	private String nom;
+	private int force;
+	
 	public Gaulois(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
 	}
-	private String nom;
-	private int force;
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + "»")
+	}
+	
+	private String prendreParole() {
+		return "Le gaulois " + nom + " : ";
+	}
+	
+	public void frapper(Romain romain) {
+		System.out.println(nom + " envoi un grand coup dans la mâchoire de " + romain.getNom());
+		romain.recevoirCoup(force / 3);
+	}
+	
+	@Override
 	private int effetPotion = -1;
+	
+	public static void main(String[] args) {
+		
+	}
+
 }
+
