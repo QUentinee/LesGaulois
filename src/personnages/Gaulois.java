a8b089c7137e70cf1407143281d422abb6557f91
QUentinee <que.heude@gmail.com>
2023-09-25 18:45:33 +0200

    Astérix qui entre en jeu

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -28,71 +28,10 @@ public class Gaulois {
 		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion="
 				+ effetPotion + "]";
 	}
+	
 	public static void main(String[] args) {
-		//TODO créer un main permettant de tester la classe Gaulois
-	}
-}
-
-public class Romain {
-	private String nom;
-	private int force;
-	
-	public Romain(String nom, int force) {
-		this.nom = nom;
-		this.force = force;
-	}
-	public String getNom() {
-		return nom;
-	}
-	public void parler(String texte) {
-		System.out.println(prendreParole() + "« " + texte + "»");
-	}
-	private String prendreParole() {
-		return "Le romain " + nom + " : ";
-	}
-	public void recevoirCoup(int forceCoup) {
-		force -= forceCoup;
-		if (force > 0) {
-			parler("Aïe");
-		} else {
-			parler("J'abandonne...");
-		}
-	}
-}
-
-public class Village {
-	private String nom;
-	
-	public Village(String nom) {
-		this.nom = nom;
-	}
-	public String getNom() {
-		return nom;
-	}
-}
-public class Chef {
-	private String nom;
-	private int force;
-	private Village village;
-	
-	public Chef(String nom, int force, Village village) {
-		this.nom = nom;
-		this.force = force;
-		this.village = village;
-	}
-	public String getNom() {
-		return nom;
-	}
-	public void parler(String texte) {
-		System.out.println(prendreParole() + "« " + texte + "»");
-	}
-	private String prendreParole() {
-		return "Le chef " + nom + " du village " + village.getNom() + " : ";
-	}
-	public void frapper(Romain romain) {
-		System.out.println(nom + " envoie un grand coup dans la mâchoire de " +
-romain.getNom());
-		romain.recevoirCoup(force / 3);
+		Gaulois Astérix = new Gaulois("Astérix",8);
+		System.out.println(Astérix);
 	}
 }
 
