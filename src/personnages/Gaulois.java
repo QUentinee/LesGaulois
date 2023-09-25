24e7c5c1e259e95318be6b38538a2f480ece396a
QUentinee <que.heude@gmail.com>
2023-09-25 18:16:29 +0200

    premier copié collé

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -3,35 +3,33 @@ package personnages;
 public class Gaulois {
 	private String nom;
 	private int force;
+	private int effetPotion = 1;
 	
 	public Gaulois(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
 	}
-	
 	public String getNom() {
 		return nom;
 	}
-	
 	public void parler(String texte) {
-		System.out.println(prendreParole() + "Â« " + texte + "Â»")
+		System.out.println(prendreParole() + "« " + texte + "»");
 	}
-	
 	private String prendreParole() {
 		return "Le gaulois " + nom + " : ";
 	}
-	
 	public void frapper(Romain romain) {
-		System.out.println(nom + " envoi un grand coup dans la mÃ¢choire de " + romain.getNom());
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de "
+				+ romain.getNom());
 		romain.recevoirCoup(force / 3);
-	}
-	
+		}
 	@Override
-	private int effetPotion = -1;
-	
-	public static void main(String[] args) {
-		
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion="
+				+ effetPotion + "]";
 	}
-
+	public static void main(String[] args) {
+		//TODO créer un main permettant de tester la classe Gaulois
+	}
 }
 
