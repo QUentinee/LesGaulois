82c8290ae9cc93dbbbd24424aa22edca0c19b5a4
QUentinee <que.heude@gmail.com>
2023-09-25 18:22:50 +0200

    dernier copié collé

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -60,5 +60,40 @@ public class Romain {
 	}
 }
 
+public class Village {
+	private String nom;
+	
+	public Village(String nom) {
+		this.nom = nom;
+	}
+	public String getNom() {
+		return nom;
+	}
+}
+public class Chef {
+	private String nom;
+	private int force;
+	private Village village;
+	
+	public Chef(String nom, int force, Village village) {
+		this.nom = nom;
+		this.force = force;
+		this.village = village;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + "»");
+	}
+	private String prendreParole() {
+		return "Le chef " + nom + " du village " + village.getNom() + " : ";
+	}
+	public void frapper(Romain romain) {
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de " +
+romain.getNom());
+		romain.recevoirCoup(force / 3);
+	}
+}
 
 
